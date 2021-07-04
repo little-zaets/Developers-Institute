fa5666380ba2b582beebeaaa4c86ea3b5932f761
Rina Volovich <rinavolovich@gmail.com>
2021-07-04 11:33:48 +0300

    formatted file structure + portfolio + week 2

---
+++ week_2/day_1/XP/Ex1.js
@@ -0,0 +1,33 @@
+//Exercise 1
+
+//Evaluate the code below:
+
+/*
+5 + "34" = 534
+5 - "4" = 1
+10 % 5 = 0
+5 % 10 = 5
+"Java" + "Script" = JavaScript
+" " + " " = "  "
+" " + 0 = 0
+true + true = 2
+true + false = 1
+false + true = 1
+false - true = -1
+3 - 4 = -1
+"Bob" - "bill" = NaN
+*/
+
+console.log(5 + "34");
+console.log(5 - "4");
+console.log(10 % 5);
+console.log(5 % 10);
+console.log("Java" + "Script");
+console.log(" " + " ");
+console.log(" " + 0);
+console.log(true + true);
+console.log(true + false);
+console.log(false + true);
+console.log(false - true);
+console.log(3 - 4);
+console.log("Bob" - "bill");
