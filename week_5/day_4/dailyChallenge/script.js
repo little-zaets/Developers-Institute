22239efdf8b7dd26f1805e3e9910b7ffcdf61a7f
Rina Volovich <rinavolovich@gmail.com>
2021-07-29 13:02:03 +0300

    week 5 day 5 miniproject-starwars

--- week_5/day_4/dailyChallenge/script.js
+++ week_5/day_4/dailyChallenge/script.js
@@ -34,3 +34,11 @@ Promise.all([
 	})
 ]).then(console.log).catch(error => console.log("REJECTED: ", error));
 
+/*const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, 'foo');
+});
+Promise.all([promise1, promise2, promise3]).then((values) => {
+    console.log(values);
+  });*/
---
+++ week_5/day_5/miniProject/script.js
@@ -0,0 +1,69 @@
+//retrieve DOM elements
+let names = document.querySelector("#name");
+let height = document.querySelector("#height");
+let gender = document.querySelector("#gender");
+let birthYear = document.querySelector("#birth-year");
+let homeWorld = document.querySelector("#home-world");
+let button = document.querySelector("#button");
+
+//retrieve api info 
+let retrieve = async() => {
+	loading();
+	let randomize = Math.floor(Math.random() * 99);
+	let apiUrl = "https://swapi.dev/api/people/" + randomize + "/";
+	try {
+		let fetched = await fetch(apiUrl);
+		if (fetched.status != 200) {
+			throw new Error("Something went wrong: ");
+		}
+		else {
+			let response = await fetched.json();
+			let planet = await getPlanet(response.homeworld);
+			displayInfo(response, planet);
+			console.log(response);
+		}
+	}
+	catch(error) {
+		displayError(error);
+		console.log("Something went wrong...");
+    }
+}
+button.addEventListener("click", retrieve);
+
+//display results
+let displayInfo = (response, planet) => {
+	names.innerText = `Name: ${response.name}`;
+	height.innerText = `Height: ${response.height}`;
+	gender.innerText = `Gender: ${response.gender}`;
+	birthYear.innerText = `Birth Year: ${response.birth_year}`;
+	homeWorld.innerText = `Home World: ${planet}`;
+}
+//loading icon after click
+function loading() {
+	names.innerHTML = '<i class="fa fa-cog fa-spin fa-2x "></i> <p>Loading...</p>';
+	height.innerText = "";
+	gender.innerText = "";
+	birthYear.innerText = "";
+}
+
+//on 404 error
+function displayError() {
+	names.innerText = "Oh No! That person isn't available.";
+	height.innerText = "";
+	gender.innerText = "";
+	birthYear.innerText = "";
+}
+
+//fetch planet via the url provided in the 	original response
+const getPlanet = async(response) => {
+	let newUrl = response;
+	let planetFetch = await fetch(newUrl);
+	if (planetFetch.status != 200) {
+		throw new Error("Something went wrong: ");
+	}
+	else {
+		let planetResp = await planetFetch.json();
+		console.log(planetResp.name);
+		return planetResp.name;
+	}
+}
